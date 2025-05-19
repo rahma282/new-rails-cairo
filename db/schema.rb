334737035a3957a2bd9539d81a2a2889032342ef
Rahma Mostafa <rahmamostafa282@gmail.com>
2025-05-20 02:51:45 +0300

    feat: rename the association from the generic user to creator

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_18_104440) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id", null: false
+    t.integer "creator_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["creator_id"], name: "index_posts_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,5 +27,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_18_104440) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "creator_id"
 end
